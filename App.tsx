0b75f35dc36b884c020108864efad703193f3c52
Abdelrahman Yasser <abdelrahman.yasser.365@gmail.com>
2024-11-01 22:10:03 +0200

    feat(App): add small description + screenshots preview

--- App.tsx
+++ App.tsx
@@ -1,117 +1,215 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import React from 'react';
-import type {PropsWithChildren} from 'react';
+import React, {useState, useEffect, useRef, useCallback} from 'react';
 import {
+  Animated,
+  Image,
+  Linking,
+  Pressable,
   SafeAreaView,
   ScrollView,
   StatusBar,
   StyleSheet,
   Text,
-  useColorScheme,
   View,
 } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+const icons = [
+  {
+    name: 'Fastlane',
+    icon: 'https://fastlane.tools/favicon.ico',
+    link: 'https://fastlane.tools/',
+  },
+  {
+    name: 'Firebase',
+    icon: 'https://firebase.google.com/favicon.ico',
+    link: 'https://firebase.google.com/',
+  },
+  {
+    name: 'CircleCI',
+    icon: 'https://circleci.com/favicon.ico',
+    link: 'https://circleci.com/developer/',
+  },
+];
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
+const screenshots = [
+  require('./screenshots/image1.png'),
+  require('./screenshots/image3.png'),
+  require('./screenshots/image2.png'),
+];
 
 function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+  const opacity = useRef(new Animated.Value(1)).current;
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+  const handleNext = useCallback(() => {
+    Animated.timing(opacity, {
+      toValue: 0,
+      duration: 500,
+      useNativeDriver: true,
+    }).start(() => {
+      setCurrentImageIndex(prevIndex => (prevIndex + 1) % screenshots.length);
+      Animated.timing(opacity, {
+        toValue: 1,
+        duration: 500,
+        useNativeDriver: true,
+      }).start();
+    });
+  }, [opacity]);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      handleNext();
+    }, 5000);
+
+    return () => clearInterval(interval);
+  }, [handleNext]);
+
+  // const handlePrevious = () => {
+  //   Animated.timing(opacity, {
+  //     toValue: 0,
+  //     duration: 500,
+  //     useNativeDriver: true,
+  //   }).start(() => {
+  //     setCurrentImageIndex(prevIndex =>
+  //       prevIndex === 0 ? screenshots.length - 1 : prevIndex - 1,
+  //     );
+  //     Animated.timing(opacity, {
+  //       toValue: 1,
+  //       duration: 500,
+  //       useNativeDriver: true,
+  //     }).start();
+  //   });
+  // };
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+    <SafeAreaView style={styles.container}>
+      <StatusBar />
+      <Text style={styles.title}>React Native CICD</Text>
+      <ScrollView contentContainerStyle={styles.container}>
+        <Text style={styles.text}>
+          Try <Text style={styles.highlightedText}>CICD</Text> for{' '}
+          <Text style={styles.highlightedText}>React Native</Text>
+          project with <Text style={styles.highlightedText}>
+            Fastlane{' '}
+          </Text>, <Text style={styles.highlightedText}>Firebase </Text>
+          ,and <Text style={styles.highlightedText}>CircleCI</Text>
+        </Text>
+        <View style={styles.icons}>
+          {icons.map(icon => (
+            <Pressable
+              key={icon.name}
+              onPress={() => {
+                Linking.openURL(icon.link);
+              }}
+              style={styles.icon}>
+              <Image source={{uri: icon.icon}} style={styles.iconImage} />
+              <Text
+                style={StyleSheet.compose(styles.text, styles.highlightedText)}>
+                {icon.name}
+              </Text>
+            </Pressable>
+          ))}
         </View>
+        <Text style={StyleSheet.compose(styles.text, styles.highlightedText)}>
+          Screenshots
+        </Text>
+        <Animated.Image
+          source={screenshots[currentImageIndex]}
+          style={[styles.screenshotImage, {opacity}]}
+        />
+        <View style={styles.indicatorContainer}>
+          {screenshots.map((_, index) => (
+            <View
+              key={index}
+              style={[
+                styles.indicator,
+                index === currentImageIndex && styles.activeIndicator,
+              ]}
+            />
+          ))}
+        </View>
+        {/* <View style={styles.buttonContainer}>
+          <Pressable style={styles.button} onPress={handlePrevious}>
+            <Text style={styles.buttonText}>Previous</Text>
+          </Pressable>
+          <Pressable style={styles.button} onPress={handleNext}>
+            <Text style={styles.buttonText}>Next</Text>
+          </Pressable>
+        </View> */}
       </ScrollView>
     </SafeAreaView>
   );
 }
-
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  container: {
+    flex: 1,
+    padding: 24,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
-  sectionTitle: {
-    fontSize: 24,
+  title: {
+    fontSize: 32,
+    fontWeight: 'bold',
+    padding: 20,
+    color: 'blue',
+  },
+  text: {
+    fontSize: 22,
+    fontWeight: '600',
+    textAlign: 'center',
+    lineHeight: 40,
+  },
+  highlightedText: {
+    color: 'blue',
+  },
+  icon: {
+    gap: 20,
+    alignItems: 'center',
+  },
+  iconImage: {
+    width: 50,
+    height: 50,
+  },
+  icons: {
+    flexDirection: 'row',
+    margin: 20,
+    gap: 30,
+  },
+  screenshotImage: {
+    width: 300,
+    height: 150,
+    marginTop: 20,
+    resizeMode: 'cover',
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    gap: 10,
+    marginTop: 20,
+  },
+  button: {
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    backgroundColor: 'blue',
+    borderRadius: 5,
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 16,
     fontWeight: '600',
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
+  indicatorContainer: {
+    flexDirection: 'row',
+    marginTop: 10,
+    gap: 10,
   },
-  highlight: {
-    fontWeight: '700',
+  indicator: {
+    width: 10,
+    height: 10,
+    borderRadius: 5,
+    backgroundColor: 'lightgray',
+  },
+  activeIndicator: {
+    width: 30,
+    backgroundColor: 'blue',
   },
 });
 
